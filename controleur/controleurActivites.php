1243cc98a4e7860427d01a7528a9fdde8e25f55c
Goncalves Mathéo <goncalves.matheo@educ-valadon-limoges.fr>
2024-10-15 11:29:06 +0200

    Update controleurActivites.php

--- controleur/controleurActivites.php
+++ controleur/controleurActivites.php
@@ -1,10 +1,6 @@
 // controleur/ControleurActivites.php
 <?php
-include_once "$racine/model/bd.php";
-include_once "$racine/model/activites.php";
-include "$racine/vue/entete.php";
-include "$racine/vue/vueActivites.php";
-include "$racine/vue/pied.php";
+
 
 class ControleurActivites {
 
