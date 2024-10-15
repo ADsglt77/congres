e68d043a4f8b519818e4f56db94c0b8efbff482c
Goncalves Mathéo <goncalves.matheo@educ-valadon-limoges.fr>
2024-10-15 11:22:51 +0200

    Update controleurActivites.php

--- controleur/controleurActivites.php
+++ controleur/controleurActivites.php
@@ -1,10 +1,50 @@
-<?php 
+// controleur/ControleurActivites.php
+<?php
 include_once "$racine/model/bd.php";
 include_once "$racine/model/activites.php";
-
-
-
 include "$racine/vue/entete.php";
 include "$racine/vue/vueActivites.php";
 include "$racine/vue/pied.php";
-?>
+
+class ControleurActivites {
+
+    public function index() {
+        $activites = Activites::getActivites();
+        include 'vue/vueActivites.php';  // Appelle la vue pour afficher la liste des activités
+    }
+
+    public function show($id) {
+        $activite = Activites::getActivite($id);
+        include 'vue/vueActiviteDetail.php';  // Appelle la vue pour afficher le détail d'une activité
+    }
+
+    public function create() {
+        include 'vue/vueAjouterActivite.php';  // Affiche le formulaire pour créer une activité
+    }
+
+    public function store() {
+        $nom = $_POST['nom'];
+        $description = $_POST['description'];
+        $prix = floatval($_POST['prix']);
+        Activites::ajouterActivite($nom, $description, $prix);
+        header('Location: /activites');  // Redirige vers la liste des activités
+    }
+
+    public function edit($id) {
+        $activite = Activites::getActivite($id);
+        include 'vue/vueModifierActivite.php';  // Affiche le formulaire pour modifier une activité
+    }
+
+    public function update($id) {
+        $nom = $_POST['nom'];
+        $description = $_POST['description'];
+        $prix = floatval($_POST['prix']);
+        Activites::modifierActivite($id, $nom, $description, $prix);
+        header('Location: /activites');
+    }
+
+    public function delete($id) {
+        Activites::supprimerActivite($id);
+        header('Location: /activites');
+    }
+}
