cb97a9fa6af30f986288cca8512188037b440b33
Goncalves Mathéo <goncalves.matheo@educ-valadon-limoges.fr>
2024-10-15 11:24:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,29 @@
+// index.php
 <?php
+
+require 'model/Activites.php';
+require 'controleur/ControleurActivites.php';
 include "./controleur/controleurPrincipal.php";
 
-if (isset($_GET["action"])){
-    $action = $_GET["action"];
-}
-else{
-    
-    $action = "defaut";
-}
+$controleur = new ControleurActivites();
 
-$fichier = controleurPrincipal($action);
-include "./controleur/$fichier";
-
-?>
+if (isset($_GET['action'])) {
+    $action = $_GET['action'];
+    if ($action == 'create') {
+        $controleur->create();
+    } elseif ($action == 'store') {
+        $controleur->store();
+    } elseif ($action == 'edit' && isset($_GET['id'])) {
+        $controleur->edit($_GET['id']);
+    } elseif ($action == 'update' && isset($_GET['id'])) {
+        $controleur->update($_GET['id']);
+    } elseif ($action == 'delete' && isset($_GET['id'])) {
+        $controleur->delete($_GET['id']);
+    } elseif ($action == 'show' && isset($_GET['id'])) {
+        $controleur->show($_GET['id']);
+    } else {
+        $controleur->index();
+    }
+} else {
+    $controleur->index();  // Par défaut, afficher la liste des activités
+}
