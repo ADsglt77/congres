f044cb27a66454a2d4dfe15f1da6909bb4eb85c7
Goncalves Mathéo <goncalves.matheo@educ-valadon-limoges.fr>
2024-11-12 09:42:12 +0100

    Update activites.php

--- model/activites.php
+++ model/activites.php
@@ -5,12 +5,16 @@ class Activites {
     private string $Nom;
     private string $description;
     private float $prix;
+    private time $heure;
+    private date $date;
 
-    public function __construct(int $unId, string $unNom, string $uneDescription, float $unPrix) {
+    public function __construct(int $unId, string $unNom, string $uneDescription, float $unPrix, time $uneHeure, date $uneDate) {
         $this->Id = $unId;
         $this->Nom = $unNom;
         $this->description = $uneDescription;
         $this->prix = $unPrix;
+        $this->heure = $uneHeure;
+        $this->date = $uneDate;
     }
 
     public function getId(): int {
@@ -44,6 +48,18 @@ class Activites {
     public function setPrix(float $nouveauPrix): void {
         $this->prix = $nouveauPrix;
     }
+    public function getHeure(): time {
+        return $this->heure;
+    }
+    public function setHeure(time $nouvelleHeure): void {
+        $this->heure = $nouvelleHeure;
+    }
+    public function getDate(): date {
+        return $this->date;
+    }
+    public function setDate(date $nouvelleDate): void {
+        $this->date = $nouvelleDate;
+    }
 
     public static function getActivites() {
         include "bd.php";
@@ -63,14 +79,14 @@ class Activites {
 
     public static function ajouterActivite() {
         include "bd.php";
-        $requete = "INSERT INTO activites (nom, description, prix) VALUES (?, ?, ?)";
+        $requete = "INSERT INTO activites (nom, description, prix) VALUES (?, ?, ?, ?, ?)";
         $stmt = $pdo->prepare($requete);
         $stmt->execute();
     }
 
     public static function modifierActivite() {
         include "bd.php";
-        $requete = "UPDATE activites SET nom = ?, description = ?, prix = ? WHERE id = ?";
+        $requete = "UPDATE activites SET nom = ?, description = ?, prix = ?, heure = ?, date = ? WHERE id = ?";
         $stmt = $pdo->prepare($requete);
         $stmt->execute();
     }
