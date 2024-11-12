3d88ceae6a930c398a1931a05bfdecceb76282bd
Goncalves Mathéo <goncalves.matheo@educ-valadon-limoges.fr>
2024-11-12 09:49:52 +0100

    Update congressiste.php

--- model/congressiste.php
+++ model/congressiste.php
@@ -1,7 +1,28 @@
 <?php
 
 class Congressistes {
+    private int $Id;
+    private string $Nom;
 
-    // faire une collection vers activité + faire fonction inscription 
+    public function __construct(int $unId, string $unNom) {
+        $this->Id = $unId;
+        $this->Nom = $unNom;
+    }
+
+    public function getId(): int {
+        return $this->Id;
+    }
+    
+    public function getNom(): string {
+        return $this->Nom;
+    }
+    public static function getcongressiste(){
+        include "bd.php";
+        $requete = "SELECT * FROM congressiste";
+        $stmt = $pdo->prepare($requete);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
 }
+
 ?>
