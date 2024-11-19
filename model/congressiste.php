4c327af8c9d5b4ea6f6195f6fa2854b7a4e4f911
ADsglt77 <149898260+ADsglt77@users.noreply.github.com>
2024-11-19 08:18:13 +0100

    aaaa

--- model/congressiste.php
+++ model/congressiste.php
@@ -3,10 +3,12 @@
 class Congressistes {
     private int $Id;
     private string $Nom;
+    private Activites $activites;
 
-    public function __construct(int $unId, string $unNom) {
+    public function __construct(int $unId, string $unNom, Activites $uneActivite) {
         $this->Id = $unId;
         $this->Nom = $unNom;
+        $this->Activites = $uneActivite;
     }
 
     public function getId(): int {
@@ -16,6 +18,11 @@ class Congressistes {
     public function getNom(): string {
         return $this->Nom;
     }
+
+    public function getActivites(): Activites {
+        return $this->Activites;
+    }
+
     public static function getcongressiste(){
         include "bd.php";
         $requete = "SELECT * FROM congressiste";
