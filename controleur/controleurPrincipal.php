a97de921b98d7cbc5431e901db2ef07d6c59fbc8
Goncalves Mathéo <goncalves.matheo@educ-valadon-limoges.fr>
2024-11-26 09:48:40 +0100

    Update controleurPrincipal.php

--- controleur/controleurPrincipal.php
+++ controleur/controleurPrincipal.php
@@ -3,13 +3,15 @@ function controleurPrincipal($action){
     $lesActions = array();
     $lesActions["activites"] = "activites.php";
     $lesActions["congressiste"] = "congressiste.php";
+    $lesActions["accueil"] = "../vue/accueil.php";
+
 
  
     if (array_key_exists ( $action , $lesActions )){
         return $lesActions[$action];
     }
     else{
-        return $lesActions["activites"];
+        return $lesActions["accueil"];
     }
 }
 ?>
