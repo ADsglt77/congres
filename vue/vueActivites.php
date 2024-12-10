97f7fd558a053a3fb36835ff22c65b0608c2febe
Goncalves Mathéo <goncalves.matheo@educ-valadon-limoges.fr>
2024-12-10 08:23:58 +0100

    Update vueActivites.php

--- vue/vueActivites.php
+++ vue/vueActivites.php
@@ -20,6 +20,8 @@
             <td style='padding: 15px;'><?php echo $activite->date_activite; ?></td>
             <td style='padding: 15px;'><?php echo $activite->heure; ?></td>
             <td style='padding: 15px;'><a href='index.php?action=activites&id=<?php echo $activite->id ?>'>SUPPRIMER</a></td>
+            <td><button onclick="openForm(<?php echo $activite->id; ?>)">Modifier</button></td>
+ 
         </tr>
     <?php } ?>
 </table>
@@ -33,6 +35,31 @@
     <input type="submit" name="submit" value="Ajouter"/>
 </form>
 
+<div id="editForm" style="display:none;">
+    <h1>Modifier Activite</h1>
+    <form action="./?action=modifierActivite" method="POST">
+        <input type="hidden" id="editId" name="id" />
+        <input type="text" id="editNom" name="nom" placeholder="Nom de l'Activité" required/><br />
+        <input type="text" id="editPrix" name="prix" placeholder="Prix de l'activité" required/><br />
+        <input type="time" id="editHeure" name="heure" required/><br />
+        <input type="date" id="editDate" name="date" required/><br />
+        <input type="submit" name="submit" value="Modifier"/>
+    </form>
+</div>
+ 
+<script>
+function openForm(id) {
+    document.getElementById('editForm').style.display = 'block';
+    // Populate the form with the activity data
+    var activite = <?php echo json_encode($LesActivites); ?>.find(a => a.id == id);
+    document.getElementById('editId').value = activite.id;
+    document.getElementById('editNom').value = activite.nom;
+    document.getElementById('editPrix').value = activite.prix;
+    document.getElementById('editHeure').value = activite.heure;
+    document.getElementById('editDate').value = activite.date_activite;
+}
+</script>
+ 
 <?php
     include "./vue/pied.php";
 ?>
