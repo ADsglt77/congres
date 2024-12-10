1cbe58b4f18129be2b6cb3955b4033ab33bf96ac
Jonathan7501 <gravelat.jonathan@etu.educ-valadon-limoges.fr>
2024-12-10 11:58:46 +0100

    Update controleurCongressiste.php

--- controleur/controleurCongressiste.php
+++ controleur/controleurCongressiste.php
@@ -1,8 +1,39 @@
 <?php
 
 include "model/congressiste.php";
+include "model/activites.php";
 
-// On récupère les congressistes etc ...
+$congressiste = new Congressiste();
+$activites = new Activites();
 
+// Variables pour le choix d'une activité et d'un congressiste
+$activiteSelectionnee = $_POST['id_activite'] ?? null;
+$congressisteSelectionne = $_POST['id_congressiste'] ?? null;
 
-include_once "vue/vueCongressiste.php";
+// Récupérer les activités et les congressistes
+$LesActivites = $activites->getLesActivites();
+$LesCongressistes = $congressiste->getTousCongressistes();
+
+// Inscription d'un congressiste à une activité
+if (isset($_POST['inscrire']) && $activiteSelectionnee && $congressisteSelectionne) {
+    $congressiste->setId($congressisteSelectionne);
+    if ($congressiste->inscrireActivite($activiteSelectionnee)) {
+        $message = "Inscription réussie.";
+    } else {
+        $message = "Erreur : Une facture existe ou le congressiste est déjà inscrit.";
+    }
+}
+
+// Annulation d'une inscription
+if (isset($_POST['annuler']) && $activiteSelectionnee && $congressisteSelectionne) {
+    $congressiste->setId($congressisteSelectionne);
+    if ($congressiste->annulerInscription($activiteSelectionnee)) {
+        $message = "Annulation réussie.";
+    } else {
+        $message = "Erreur : Annulation impossible. Une facture existe peut-être.";
+    }
+}
+
+include "vue/vueCongressiste.php";
+?>
+
